9f86fc4cb770125b202ee4a4f8d469818b6e09d8
김형익 <kiomnd2@naver.com>
2024-09-12 14:23:26 +0900

    create echo service

--- src/main/java/telegram/telegramremotehandler/TelegramRemoteHandlerApplication.java
+++ src/main/java/telegram/telegramremotehandler/TelegramRemoteHandlerApplication.java
@@ -2,12 +2,20 @@ package telegram.telegramremotehandler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import telegram.telegramremotehandler.interfaces.handler.TelegramBotHandler;
 
 @SpringBootApplication
 public class TelegramRemoteHandlerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TelegramRemoteHandlerApplication.class, args);
+    public static void main(String[] args) throws TelegramApiException {
+        ConfigurableApplicationContext context = SpringApplication.run(TelegramRemoteHandlerApplication.class, args);
+        TelegramBotsApi bot = new TelegramBotsApi(DefaultBotSession.class);
+        TelegramBotHandler handler = context.getBean(TelegramBotHandler.class);
+        bot.registerBot(handler);
     }
 
 }
--- src/main/java/telegram/telegramremotehandler/interfaces/handler/TelegramBotHandler.java
+++ src/main/java/telegram/telegramremotehandler/interfaces/handler/TelegramBotHandler.java
@@ -1,28 +1,58 @@
 package telegram.telegramremotehandler.interfaces.handler;
 
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class TelegramBotHandler extends TelegramWebhookBot {
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class TelegramBotHandler extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
 
     @Override
     public String getBotToken() {
-        return "";
-    }
-
-    @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
-    }
-
-    @Override
-    public String getBotPath() {
-        return "";
+        return botToken;
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return botUsername;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String receivedText = update.getMessage().getText();
+            String chatId = String.valueOf(update.getMessage().getChatId());
+            System.out.println(receivedText);
+            System.out.println(chatId);
+            if ("/myChatId".equals(receivedText)) {
+                resMessage(chatId, "chatId: " + chatId);
+            }
+        }
+    }
+
+    private void resMessage(String chatId, String text) {
+        SendMessage responseMessage = new SendMessage();
+        responseMessage.setChatId(chatId);
+        responseMessage.setText(text);
+        try {
+            execute(responseMessage);
+        } catch (Exception e) {
+            e.printStackTrace(); // 전송 중 예외 발생 시 예외를 출력
+        }
     }
 }
